e6d56ffa1c728cb4618ed40793530607a1dae264
Bachan Ghatuary <46030606+bachan143@users.noreply.github.com>
2023-07-18 13:05:54 +0530

    first commit

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export { ImageCropperModule } from './src/lib/image-cropper/image-cropper.module';
+export { ImageCropperComponent } from './src/lib/image-cropper/image-cropper.component';
+export { ImageCropper } from './src/lib/image-cropper/imageCropper';
+export { CropperSettings } from './src/lib/image-cropper/cropper-settings';
+export { CropperDrawSettings } from './src/lib/image-cropper/cropper-draw-settings';
+export { Bounds } from './src/lib/image-cropper/model/bounds';
+export { CropPosition } from './src/lib/image-cropper/model/cropPosition';
