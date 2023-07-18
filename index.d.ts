a66e82d6214236136e31a7f0bffedefc058ed0cc
Bachan Ghatuary <46030606+bachan143@users.noreply.github.com>
2023-07-18 13:13:18 +0530

    first commit

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,6 @@
-export { ImageCropperModule } from './src/lib/image-cropper/image-cropper.module';
-export { ImageCropperComponent } from './src/lib/image-cropper/image-cropper.component';
-export { ImageCropper } from './src/lib/image-cropper/imageCropper';
-export { CropperSettings } from './src/lib/image-cropper/cropper-settings';
-export { CropperDrawSettings } from './src/lib/image-cropper/cropper-draw-settings';
-export { Bounds } from './src/lib/image-cropper/model/bounds';
-export { CropPosition } from './src/lib/image-cropper/model/cropPosition';
+/**
+ * Generated bundle index. Do not edit.
+ */
+/// <amd-module name="ngx-img-cropper" />
+export * from './src/public_api';
+//# sourceMappingURL=ngx-img-cropper.d.ts.map
